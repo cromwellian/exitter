aac789bbb871fc2f32417d4e609f839a6bb007ff
Ray Cromwell <cromwellian@gmail.com>
2023-07-26 00:03:23 -0700

    Create content.js

---
+++ content.js
@@ -0,0 +1,6 @@
+
+    var link = document.querySelector('link[rel="shortcut icon"]');
+    if (link) {
+        link.href = 'https://abs.twimg.com/responsive-web/client-web/icon-ios.b1fc727a.png';
+    }
+    
